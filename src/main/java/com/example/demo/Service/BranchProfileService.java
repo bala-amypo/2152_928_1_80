6adbd64acfaaf52fb8f3ad95632c2c8487474fb1
bala-amypo <bala@amypo.in>
2025-12-19 03:48:42 +0000

    User commit

--- src/main/java/com/example/demo/Service/BranchProfileService.java
+++ src/main/java/com/example/demo/Service/BranchProfileService.java
@@ -1,45 +1,17 @@
 package com.example.demo.Service;
 
 import java.util.List;
-import org.springframework.stereotype.Service;
 import com.example.demo.Entity.BranchProfileEntity;
-import com.example.demo.Repository.BranchProfileRepository;
 
-@Service
-public class BranchProfileServiceImpl implements BranchProfileService {
+public interface BranchProfileService {
 
-    private final BranchProfileRepository repo;
+    BranchProfileEntity createBranch(BranchProfileEntity branch);
 
-    public BranchProfileServiceImpl(BranchProfileRepository repo) {
-        this.repo = repo;
-    }
+    BranchProfileEntity updateBranchStatus(Long id, boolean active);
 
-    @Override
-    public BranchProfileEntity createBranch(BranchProfileEntity branch) {
-        return repo.save(branch);
-    }
+    List<BranchProfileEntity> getAllBranches();
 
-    @Override
-    public BranchProfileEntity updateBranchStatus(Long id, boolean active) {
-        BranchProfileEntity branch = getBranchById(id);
-        branch.setActive(active);
-        return repo.save(branch);
-    }
+    BranchProfileEntity getBranchById(Long id);
 
-    @Override
-    public List<BranchProfileEntity> getAllBranches() {
-        return repo.findAll();
-    }
-
-    @Override
-    public BranchProfileEntity getBranchById(Long id) {
-        return repo.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Branch not found"));
-    }
-
-    @Override
-    public BranchProfileEntity findByBranchCode(String branchCode) {
-        return repo.findByBranchCode(branchCode)
-                .orElseThrow(() -> new ResourceNotFoundException("Branch code not found"));
-    }
+    BranchProfile findByBranchCode(String branchCode);
 }
