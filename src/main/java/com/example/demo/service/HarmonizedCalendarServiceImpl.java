33a66b0c40304ea18ee769119922630d791cc32f
bala-amypo <bala@amypo.in>
2025-12-19 10:02:38 +0000

    User commit

--- src/main/java/com/example/demo/service/HarmonizedCalendarServiceImpl.java
+++ src/main/java/com/example/demo/service/HarmonizedCalendarServiceImpl.java
@@ -11,19 +11,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class HarmonizedCalendarServiceImpl
-        implements HarmonizedCalendarService {
+public class HarmonizedCalendarServiceImpl implements HarmonizedCalendarService {
 
     private final HarmonizedCalendarRepository repository;
 
-    public HarmonizedCalendarServiceImpl(
-            HarmonizedCalendarRepository repository) {
+    public HarmonizedCalendarServiceImpl(HarmonizedCalendarRepository repository) {
         this.repository = repository;
     }
 
     @Override
-    public HarmonizedCalendarEntity generateHarmonizedCalendar(
-            String title, String generatedBy) {
+    public HarmonizedCalendarEntity generateHarmonizedCalendar(String title, String generatedBy) {
 
         HarmonizedCalendarEntity calendar = new HarmonizedCalendarEntity();
         calendar.setTitle(title);
@@ -34,9 +31,7 @@ public class HarmonizedCalendarServiceImpl
 
     @Override
     public HarmonizedCalendarEntity getCalendarById(Long id) {
-        return repository.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Calendar not found"));
+        return repository.findById(id).orElseThrow(() ->new ResourceNotFoundException("Calendar not found"));
     }
 
     @Override
@@ -45,13 +40,9 @@ public class HarmonizedCalendarServiceImpl
     }
 
     @Override
-    public List<HarmonizedCalendarEntity> getCalendarsWithinRange(
-            LocalDate start, LocalDate end) {
-
-        return repository.findAll().stream()
-                .filter(c ->
-                        !c.getEffectiveFrom().isBefore(start)
-                     && !c.getEffectiveTo().isAfter(end))
-                .collect(Collectors.toList());
+    public List<HarmonizedCalendarEntity> getCalendarsWithinRange(LocalDate start, LocalDate end) {
+    return repository.findAll().stream().filter(c -> !c.getEffectiveFrom().isBefore(start) && !c.getEffectiveTo().isAfter(end)).collect(Collectors.toList());
     }
 }
+
+
