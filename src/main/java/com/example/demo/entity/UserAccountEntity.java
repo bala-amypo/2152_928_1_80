39e67e5c5a96e5496a423a2e63b23d9be5f344a9
bala-amypo <bala@amypo.in>
2025-12-19 10:14:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccountEntity.java
+++ src/main/java/com/example/demo/entity/UserAccountEntity.java
@@ -17,6 +17,16 @@ public class UserAccountEntity {
     private String role;
     private String department;
     private LocalDateTime createdAt;
+    private boolean active;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public boolean isActive() { return active; }
+    public void setActive(boolean active) { this.active = active; }
 
     @PrePersist
     void onCreate() {
