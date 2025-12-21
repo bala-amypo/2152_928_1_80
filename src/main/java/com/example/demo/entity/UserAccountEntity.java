3a9f4c49e8704b402cf2f398f20b2942d28ea372
bala-amypo <bala@amypo.in>
2025-12-21 21:04:48 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccountEntity.java
+++ src/main/java/com/example/demo/entity/UserAccountEntity.java
@@ -22,7 +22,6 @@ public class UserAccountEntity {
     @Email(message = "Invalid email")
     private String email;
 
-    // password optional now
     @JsonIgnore
     private String password;
 
@@ -44,7 +43,7 @@ public class UserAccountEntity {
         }
     }
 
-    // Getters & Setters
+    // getters/setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
