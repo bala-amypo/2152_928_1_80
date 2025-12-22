de6f868abdff8cb2cf9609e8c207ccf78dacebde
bala-amypo <bala@amypo.in>
2025-12-22 10:42:16 +0000

    User commit

--- src/main/java/com/example/demo/entity/AcademicEventEntity.java
+++ src/main/java/com/example/demo/entity/AcademicEventEntity.java
@@ -46,13 +46,26 @@ public class AcademicEventEntity {
     }
 
     // --- getters + setters ---
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-    public BranchProfileEntity getBranch() { return branch; }
-    public void setBranch(BranchProfileEntity branch) { this.branch = branch; }
-    public String getEventType() { return eventType; }
+    public Long getId() {
+         return id;
+    }
+    public void setId(Long id) { 
+        this.id = id; 
+    }
+    public String getTitle() { 
+        return title; 
+    }
+    public void setTitle(String title) {
+         this.title = title; 
+    }
+    public BranchProfileEntity getBranch() { 
+        return branch; 
+    }
+    public void setBranch(BranchProfileEntity branch) {
+        this.branch = branch;
+    }
+    public String getEventType() {
+        return eventType; }
     public void setEventType(String eventType) { this.eventType = eventType; }
     public LocalDate getStartDate() { return startDate; }
     public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
