8b90e76ef8f89703fd16a6b57e1d68057f03dd63
bala-amypo <bala@amypo.in>
2025-12-26 10:13:51 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -4,32 +4,39 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
+@Bean
+public OpenAPI openAPI() {
 
-        final String securitySchemeName = "bearerAuth";
+final String securitySchemeName = "bearerAuth";
 
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Multi-Branch Academic Calendar Harmonizer")
-                        .description("Centralized system for harmonizing academic calendars across branches")
-                        .version("1.0.0"))
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                .components(
-                        new io.swagger.v3.oas.models.Components()
-                                .addSecuritySchemes(securitySchemeName,
-                                        new SecurityScheme()
-                                                .name(securitySchemeName)
-                                                .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("JWT")
-                                )
-                );
-    }
+return new OpenAPI()
+.info(new Info()
+.title("Multi-Branch Academic Calendar Harmonizer")
+.description("Centralized system for harmonizing academic calendars across branches")
+.version("1.0.0")
+)
+.addServersItem(
+new Server()
+.url("https://9166.408procr.amypo.ai/")
+.description("Production Server")
+)
+.addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+.components(
+new io.swagger.v3.oas.models.Components()
+.addSecuritySchemes(securitySchemeName,
+new SecurityScheme()
+.name(securitySchemeName)
+.type(SecurityScheme.Type.HTTP)
+.scheme("bearer")
+.bearerFormat("JWT")
+)
+);
+}
 }
